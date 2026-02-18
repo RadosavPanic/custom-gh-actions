b4fda04650c9d10fdf97099c4bfc9db75cfc688f
RadosavPanic <radosav.panic01@gmail.com>
2026-02-18 19:21:44 +0100

    added inputs to action and obtaining inputs for aws parameters for deployment

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+// const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 function run() {
+  // 1) Get input parameters
+  core.notice("Obtaining AWS S3 Parameters...");
+  const s3BucketName = core.getInput("s3_bucket_name", { required: true });
+  const s3Region = core.getInput("s3_region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
   core.notice("Starting deployment to AWS S3...");
+
+  // 2) Upload files
+  const s3Uri = `s3://${s3BucketName}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${s3Region}`);
 
   setTimeout(() => {
     core.notice("Deployment to AWS S3 completed successfully!");
