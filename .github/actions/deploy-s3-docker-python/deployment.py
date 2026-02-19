1816edbdbe64c4715a64644c2755599a9e6e78b9
RadosavPanic <radosav.panic@gmail.com>
2026-02-19 14:17:42 +0100

    added custom docker-python gh action

---
+++ .github/actions/deploy-s3-docker-python/deployment.py
@@ -0,0 +1,31 @@
+import os
+import boto3
+import mimetypes
+from botocore.config import Config
+
+
+def run():
+    s3_bucket_name = os.environ['INPUT_S3_BUCKET_NAME']
+    s3_region = os.environ['INPUT_S3_REGION']
+    dist_folder = os.environ['INPUT_DIST_FOLDER']
+
+    configuration = Config(region_name=s3_region)
+
+    s3_client = boto3.client('s3', config=configuration)
+
+    for root, subdirs, files in os.walk(dist_folder):
+        for file in files:
+            s3_client.upload_file(
+                os.path.join(root, file),
+                s3_bucket_name,
+                os.path.join(root, file).replace(dist_folder + '/', ''),
+                ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
+            )
+
+    website_url = f'http://{s3_bucket_name}.s3-website-{s3_region}.amazonaws.com'
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+        print(f'website-url={website_url}', file=gh_output)
+
+
+if __name__ == '__main__':
+    run()
